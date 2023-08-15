ec83ab9a457fac9ca1695ac636b7be3b99d062e3
Yurekan <75236983+yurekan@users.noreply.github.com>
2023-08-15 18:10:22 +0000

    My fisrt program

--- server.py
+++ server.py
@@ -1,19 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.json
+    if "a" in data and "b" in data:
+        a = data["a"]
+        b = data["b"]
+        result = a + b
+        return result = {"result" : result}
+    else:
+        return result = {"result" : "Missing values"}
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.json
+    if "a" in data and "b" in data:
+        a = data["a"]
+        b = data["b"]
+        result = a - b
+        return result = {"result" : result}
+    else:
+        return result = {"result" : "Missing values"}
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
